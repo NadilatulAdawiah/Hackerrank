4359d70d4c8aecb1756cf5e75331d2e942108963
Nadilatul Adawiah <athulawiah026@gmail.com>
2022-01-29 14:44:38 +0700

    latihan 2

---
+++ latihan2.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class latihan2 {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("inputkan 3 nilai: ");
+        int a = scan.nextInt();
+        int b = scan.nextInt();
+        int c = scan.nextInt();
+
+        
+        System.out.println("Berikut hasil inputan: ");
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+
+    }
+    
+}
