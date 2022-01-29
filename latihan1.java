ef0c0a87341f7b1dcd47b912c876859d65d8579e
Nadilatul Adawiah <athulawiah026@gmail.com>
2022-01-29 14:09:33 +0700

    latihan 1 Day 1

---
+++ latihan1.java
@@ -0,0 +1,9 @@
+
+public class latihan1 {
+
+    public static void main(String[] args){
+        System.out.println("Hello, World.");
+        System.out.println("Hello, Java.");
+        
+    }
+}
