72768efed46491632840b6899262b679fb790dac
Nadilatul Adawiah <athulawiah026@gmail.com>
2022-02-11 15:38:16 +0800

    commit hari ke 8

---
+++ Day8_JavaRegex_ValidUsername.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Day8_JavaRegex_ValidUsername {
+    public static final String regularExpression = "^[a-zA-Z]\\w{7,29}$";
+
+    private static final Scanner scan = new Scanner(System.in);
+    
+    public static void main(String[] args) {
+        int n = Integer.parseInt(scan.nextLine());
+        while (n-- != 0) {
+            String userName = scan.nextLine();
+
+            if (userName.matches(Day8_JavaRegex_ValidUsername.regularExpression)) {
+                System.out.println("Valid");
+            } else {
+                System.out.println("Invalid");
+            }           
+        }
+    }
+}
+
